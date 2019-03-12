896bcbeafa233c44c1accec319f4d897239b3b37
flavioespinoza <flavio.espinoza@gmail.com>
2019-03-11 18:59:30 -0600

    added_index

---
+++ index.js
@@ -0,0 +1,28 @@
+/** Detect free variable `global` from Node.js. */
+var freeGlobal = typeof global == 'object' && global && global.Object === Object && global
+
+/** Detect free variable `self`. */
+var freeSelf = typeof self == 'object' && self && self.Object === Object && self
+
+/** Used as a reference to the global object. */
+var root = freeGlobal || freeSelf || Function('return this')()
+
+/**
+ * Gets the timestamp of the number of milliseconds that have elapsed since
+ * the Unix epoch (1 January 1970 00:00:00 UTC).
+ *
+ * @static
+ * @memberOf _
+ * @since 1.0.0
+ * @category Date
+ * @returns {number} Returns the timestamp.
+ * @example
+ *
+ * console.log(timestamp()) // => 1552351582644
+ * // => Logs the number of milliseconds it took for the deferred invocation.
+ */
+var timestamp = function() {
+    return root.Date.now()
+}
+
+module.exports = timestamp
