3e648a5efeae8bd38c0cfa00cd5b60317e1afe5c
Flavio Espinoza <flavio.espinoza@gmail.com>
2021-06-07 22:43:19 -0600

    Update index.js

--- methods/index.js
+++ methods/index.js
@@ -1,4 +1,3 @@
-export {default as CallToActionBlock} from "./CallToActionBlock";
 export {default as add} from "./add";
 export {default as addDays} from "./addDays";
 export {default as addHours} from "./addHours";
